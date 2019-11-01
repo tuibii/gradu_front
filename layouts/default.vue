93b87bb410a580476ad8d03b94d02b66f2ef9db9
wuyiliang <tuibii@163.com>
2019-11-01 14:10:25 +0800

    init

---
+++ layouts/default.vue
@@ -0,0 +1,95 @@
+<srript src="../assets/js/jquery.js"></srript>
+<srript src="../assets/js/main.js"></srript>
+<template>
+  <div>
+      <header class="trasparent_nav">
+          <div class="wrapper">
+            <div class="logo">
+              <a href="#"><img src="../assets/img/博砚.png" style="width: 180px;height: 68px;"/></a>
+            </div>
+            <nav>
+              <ul>
+                <li><a href="#">首页</a></li>
+                <li><a href="#">树洞广场</a></li>
+                <li><a href="#">文章</a></li>
+                <li><a href="#">问答</a></li>
+                <li><a href="#">社团</a></li>
+              </ul>
+            </nav>
+          </div>
+      </header>
+<!--      <header class="fixed_nav">-->
+<!--        <div class="wrapper">-->
+<!--          <div class="logo">-->
+<!--            <a href="#"><img src="../assets/img/博砚.png" style="width: 100px;height: 100px;"/></a>-->
+<!--          </div>-->
+<!--          <nav>-->
+<!--            <ul>-->
+<!--              <li><a href="#">首页</a></li>-->
+<!--              <li><a href="#">树洞广场</a></li>-->
+<!--              <li><a href="#">文章</a></li>-->
+<!--              <li><a href="#">问答</a></li>-->
+<!--              <li><a href="#">社团</a></li>-->
+<!--            </ul>-->
+<!--          </nav>-->
+<!--        </div>-->
+<!--      </header>-->
+      <nuxt/>
+      <footer>
+        <div class="wrapper">
+          <section class="cta cta_footer">
+            <p>Have a project in mind?  We would love to hear from you!</p>
+            <a href="#">Get in touch with us</a>
+          </section>
+
+          <div class="footer_widget">
+            <div class="f_cols">
+              <h3>Location</h3>
+              <p>3301 New Mexico Avenue Washington, DC 20016 <span class="phone">(202) 537-0787</span></p>
+              <ul class="sm">
+                <li><a class="fb" href="#"></a></li>
+                <li><a class="twitter" href="#"></a></li>
+                <li><a class="dribbble" href="#"></a></li>
+              </ul>
+            </div>
+
+            <div class="f_cols">
+              <h3>Company</h3>
+              <ul>
+                <li><a href="#">Our Story</a></li>
+                <li><a href="#">Mission</a></li>
+                <li><a href="#">Journal</a></li>
+                <li><a href="#">Careers</a></li>
+              </ul>
+            </div>
+
+            <div class="f_cols">
+              <h3>Support</h3>
+              <ul>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Policies</a></li>
+              </ul>
+            </div>
+
+            <div class="f_cols">
+              <h3>Fertile</h3>
+              <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia est eserunt mollit anim laborum.</p>
+            </div>
+          </div>
+
+          <p class="rights">© 2014 Fertile  -  All Rights Reserved  - More Templates <a href="http://www.cssmoban.com/" target="_blank" title="模板之家">模板之家</a> - Collect from <a href="http://www.cssmoban.com/" title="网页模板" target="_blank">网页模板</a></p>
+
+        </div>
+      </footer>
+  </div>
+</template>
+<style>
+  @import '../assets/css/reset.css';
+  @import '../assets/css/main.css';
+</style>
+<script>
+export default {
+
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+  <section class="billboard"></section><!-- End billboard -->
+  <section class="billboard"></section><!-- End billboard -->
+  <section class="billboard"></section><!-- End billboard -->
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+  <h1>hello</h1>
+</div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
