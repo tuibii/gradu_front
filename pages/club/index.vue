0eb5e047bfd3c929540b1483b9d22d7f393fe974
wuyiliang <tuibii@163.com>
2019-11-08 00:45:45 +0800

    init

---
+++ pages/club/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-container>
+    <el-header>
+
+    </el-header>
+  </el-container>
+</template>
+<script>
+  export default {
+
+  }
+</script>
--- pages/manager/index.vue
+++ pages/manager/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    manager
-  </div>
+  <el-container style="margin-top: 110px;">
+    <el-header style="width: 100%;height: 300px;background-color: #B3C0D1;display: flex;flex-direction:column;justify-content: space-around;align-items: center;">
+      <el-avatar style="width: 100px;height: 100px;"> admin </el-avatar>
+      <div>admin</div>
+      <div>159关注粉丝</div>
+    </el-header>
+  </el-container>
 </template>
 <script>
   export default {
