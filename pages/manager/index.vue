8c5b995c92a3ad53b074453d0499b9ec3fc6e309
wuyiliang <tuibii@163.com>
2019-11-08 17:30:23 +0800

    注册重构

--- pages/manager/index.vue
+++ pages/manager/index.vue
@@ -1,10 +1,32 @@
 <template>
-  <el-container style="margin-top: 110px;">
-    <el-header style="width: 100%;height: 300px;background-color: #B3C0D1;display: flex;flex-direction:column;justify-content: space-around;align-items: center;">
-      <el-avatar style="width: 100px;height: 100px;"> admin </el-avatar>
+  <el-container style="margin-top: 100px;flex-direction:column;align-items: center;">
+    <el-header style="width: 800px;height: 180px;background-color: #B3C0D1;display: flex;flex-direction:column;justify-content: space-around;align-items: center;">
+      <el-avatar style="width: 80px;height: 80px;"> admin </el-avatar>
       <div>admin</div>
-      <div>159关注粉丝</div>
+      <div>159关注  100粉丝</div>
     </el-header>
+    <el-container style="display: flex;flex-direction:row;justify-content: left;width: 100%;height: 800px;">
+      <el-aside>
+        <el-menu :default-openeds="['1']">
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-message"></i>导航一</template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main style="background-color: #1ba157;"></el-main>
+    </el-container>
   </el-container>
 </template>
 <script>
--- pages/treehole/index.vue
+++ pages/treehole/index.vue
@@ -5,20 +5,27 @@
         <div class="tc-data-list">
           <div class="tc-list">
             <ul class="detail-list">
+
               <li class="qa-item">
                 <div class="fl record">
                   <div class="number">
-                    <div class="border useful">
+                    <div class="border answer">
                       <p class="usenum"><a href="#" class="zan"><i class="fa fa-thumbs-up " aria-hidden="true"></i></a></p>
                       <p class="zannum"> 11 </p>
-                    </div>
-                    <div class="border answer">
-                      <a href="#" class="star"><i class="fa fa-star-o" aria-hidden="true"></i></a>
                     </div>
                   </div>
                 </div>
                 <div class="info">
-                  <p class="text"> <a href="./spit-detail.html" target="_blank"> 面试说通过了要回去等offer，现在都两周了，还说在ceo审批，两天联系hr一次，都说还在等审批什么情况？工资什么的都谈好了，也要了工资证明。面试说通过了，要回去等offer，现在都两周了还说在ceo审批，两天联系hr一次，都说还在等审批，什么情况？？工资什么的都谈好了，也要了工资证明。也工资证明。 </a> </p>
+                  <p class="text">
+                    <a href="./spit-detail.html" target="_blank"> 面试说通过了要回去等offer，现在都两周了，还说在ceo审批，两天联系hr一次，都说还在等审批什么情况？工资什么的都谈好了，也要了工资证明。面试说通过了，要回去等offer，现在都两周了还说在ceo审批，两天联系hr一次，都说还在等审批，什么情况？？工资什么的都谈好了，也要了工资证明。也工资证明。 </a>
+                  </p>
+                  <div>
+                    <el-image :src="url" style="width: 100px; height: 100px">
+                      <div slot="placeholder" class="image-slot">
+                        加载中<span class="dot">...</span>
+                      </div>
+                    </el-image>
+                  </div>
                   <div class="other">
                     <div class="fl date">
                       <span>2017-7-23 12:02</span>
@@ -30,181 +37,7 @@
                   </div>
                 </div>
                 <div class="clearfix"></div> </li>
-              <li class="qa-item">
-                <div class="fl record">
-                  <div class="number">
-                    <div class="border useful">
-                      <p class="usenum"><a href="#" class="zan"><i class="fa fa-thumbs-up " aria-hidden="true"></i></a></p>
-                      <p class="zannum"> 10 </p>
-                    </div>
-                    <div class="border answer">
-                      <a href="#" class="star"><i class="fa fa-star-o" aria-hidden="true"></i></a>
-                    </div>
-                  </div>
-                </div>
-                <div class="info">
-                  <p class="text"> <a href="./spit-detail.html" target="_blank"> 面试说通过了要回去等offer，现在都两周了，还说在ceo审批，两天联系hr一次，都说还在等审批什么情况？工资什么的都谈好了，也要了工资证明。面试说通过了，要回去等offer，现在都两周了还说在ceo审批，两天联系hr一次，都说还在等审批，什么情况？？工资什么的都谈好了，也要了工资证明。也工资证明。 </a> </p>
-                  <div class="other">
-                    <div class="fl date">
-                      <span>2017-6-22 12:02</span>
-                    </div>
-                    <div class="fr remark">
-                      <a href="#" data-toggle="modal" data-target="#shareModal" class="share"><i class="fa fa-share-alt" aria-hidden="true"></i> 分享</a>
-                      <a href="#" data-toggle="modal" data-target="#remarkModal" class="comment"><i class="fa fa-commenting" aria-hidden="true"></i> 回复</a>
-                    </div>
-                  </div>
-                </div>
-                <div class="clearfix"></div> </li>
-              <li class="qa-item">
-                <div class="fl record">
-                  <div class="number">
-                    <div class="border useful">
-                      <p class="usenum"><a href="#" class="zan"><i class="fa fa-thumbs-up " aria-hidden="true"></i></a></p>
-                      <p class="zannum"> 12 </p>
-                    </div>
-                    <div class="border answer">
-                      <a href="#" class="star"><i class="fa fa-star-o" aria-hidden="true"></i></a>
-                    </div>
-                  </div>
-                </div>
-                <div class="info">
-                  <p class="text"> <a href="./spit-detail.html" target="_blank"> 面试说通过了要回去等offer，现在都两周了，还说在ceo审批，两天联系hr一次，都说还在等审批什么情况？工资什么的都谈好了，也要了工资证明。面试说通过了，要回去等offer，现在都两周了还说在ceo审批，两天联系hr一次，都说还在等审批，什么情况？？工资什么的都谈好了，也要了工资证明。也工资证明。 </a> </p>
-                  <div class="other">
-                    <div class="fl date">
-                      <span>2017-6-22 12:02</span>
-                    </div>
-                    <div class="fr remark">
-                      <a href="#" data-toggle="modal" data-target="#shareModal" class="share"><i class="fa fa-share-alt" aria-hidden="true"></i> 分享</a>
-                      <a href="#" data-toggle="modal" data-target="#remarkModal" class="comment"><i class="fa fa-commenting" aria-hidden="true"></i> 回复</a>
-                    </div>
-                  </div>
-                </div>
-                <div class="clearfix"></div> </li>
-              <li class="qa-item">
-                <div class="fl record">
-                  <div class="number">
-                    <div class="border useful">
-                      <p class="usenum"><a href="#" class="zan"><i class="fa fa-thumbs-up " aria-hidden="true"></i></a></p>
-                      <p class="zannum"> 10 </p>
-                    </div>
-                    <div class="border answer">
-                      <a href="#" class="star"><i class="fa fa-star-o" aria-hidden="true"></i></a>
-                    </div>
-                  </div>
-                </div>
-                <div class="info">
-                  <p class="text"> <a href="./spit-detail.html" target="_blank"> 面试说通过了要回去等offer，现在都两周了，还说在ceo审批，两天联系hr一次，都说还在等审批什么情况？工资什么的都谈好了，也要了工资证明。面试说通过了，要回去等offer，现在都两周了还说在ceo审批，两天联系hr一次，都说还在等审批，什么情况？？工资什么的都谈好了，也要了工资证明。也工资证明。 </a> </p>
-                  <div class="other">
-                    <div class="fl date">
-                      <span>2017-4-22 12:02</span>
-                    </div>
-                    <div class="fr remark">
-                      <a href="#" data-toggle="modal" data-target="#shareModal" class="share"><i class="fa fa-share-alt" aria-hidden="true"></i> 分享</a>
-                      <a href="#" data-toggle="modal" data-target="#remarkModal" class="comment"><i class="fa fa-commenting" aria-hidden="true"></i> 回复</a>
-                    </div>
-                  </div>
-                </div>
-                <div class="clearfix"></div> </li>
-              <li class="qa-item">
-                <div class="fl record">
-                  <div class="number">
-                    <div class="border useful">
-                      <p class="usenum"><a href="#" class="zan"><i class="fa fa-thumbs-up " aria-hidden="true"></i></a></p>
-                      <p class="zannum"> 10 </p>
-                    </div>
-                    <div class="border answer">
-                      <a href="#" class="star"><i class="fa fa-star-o" aria-hidden="true"></i></a>
-                    </div>
-                  </div>
-                </div>
-                <div class="info">
-                  <p class="text"> <a href="./spit-detail.html" target="_blank"> 面试说通过了要回去等offer，现在都两周了，还说在ceo审批，两天联系hr一次。面试说通过了，要回去等offer，现在都两周了还说在ceo审批，两天联系hr一次，都说还在等审批，什么情况？？工资什么的都谈好了，也要了工资证明。也工资证明。 </a> </p>
-                  <div class="other">
-                    <div class="fl date">
-                      <span>2017-1-22 12:02</span>
-                    </div>
-                    <div class="fr remark">
-                      <a href="#" data-toggle="modal" data-target="#shareModal" class="share"><i class="fa fa-share-alt" aria-hidden="true"></i> 分享</a>
-                      <a href="#" data-toggle="modal" data-target="#remarkModal" class="comment"><i class="fa fa-commenting" aria-hidden="true"></i> 回复</a>
-                    </div>
-                  </div>
-                </div>
-                <div class="clearfix"></div> </li>
-              <li class="qa-item">
-                <div class="fl record">
-                  <div class="number">
-                    <div class="border useful">
-                      <p class="usenum"><a href="#" class="zan"><i class="fa fa-thumbs-up " aria-hidden="true"></i></a></p>
-                      <p class="zannum"> 12 </p>
-                    </div>
-                    <div class="border answer">
-                      <a href="#" class="star"><i class="fa fa-star-o" aria-hidden="true"></i></a>
-                    </div>
-                  </div>
-                </div>
-                <div class="info">
-                  <p class="text"> <a href="./spit-detail.html" target="_blank"> 面试说通过了要回去等offer，现在都两周了，还说在ceo审批，两天联系hr一次，都说还在等审批什么情况？工资什么的都谈好了，也要了工资证明。面试说通过了，要回去等offer，现在都两周了还说在ceo审批，两天联系hr一次，都说还在等审批，什么情况？？工资什么的都谈好了，也要了工资证明。也工资证明。 </a> </p>
-                  <div class="other">
-                    <div class="fl date">
-                      <span>2017-6-22 12:02</span>
-                    </div>
-                    <div class="fr remark">
-                      <a href="#" data-toggle="modal" data-target="#shareModal" class="share"><i class="fa fa-share-alt" aria-hidden="true"></i> 分享</a>
-                      <a href="#" data-toggle="modal" data-target="#remarkModal" class="comment"><i class="fa fa-commenting" aria-hidden="true"></i> 回复</a>
-                    </div>
-                  </div>
-                </div>
-                <div class="clearfix"></div> </li>
-              <li class="qa-item">
-                <div class="fl record">
-                  <div class="number">
-                    <div class="border useful">
-                      <p class="usenum"><a href="#" class="zan"><i class="fa fa-thumbs-up " aria-hidden="true"></i></a></p>
-                      <p class="zannum"> 10 </p>
-                    </div>
-                    <div class="border answer">
-                      <a href="#" class="star"><i class="fa fa-star-o" aria-hidden="true"></i></a>
-                    </div>
-                  </div>
-                </div>
-                <div class="info">
-                  <p class="text"> <a href="./spit-detail.html" target="_blank"> 面试说通过了要回去等offer，现在都两周了，还说在ceo审批，两天联系hr一次，都说还在等审批什么情况？工资什么的都谈好了，也要了工资证明。面试说通过了，要回去等offer，现在都两周了还说在ceo审批，两天联系hr一次，都说还在等审批，什么情况？？工资什么的都谈好了，也要了工资证明。也工资证明。 </a> </p>
-                  <div class="other">
-                    <div class="fl date">
-                      <span>2017-4-22 12:02</span>
-                    </div>
-                    <div class="fr remark">
-                      <a href="#" data-toggle="modal" data-target="#shareModal" class="share"><i class="fa fa-share-alt" aria-hidden="true"></i> 分享</a>
-                      <a href="#" data-toggle="modal" data-target="#remarkModal" class="comment"><i class="fa fa-commenting" aria-hidden="true"></i> 回复</a>
-                    </div>
-                  </div>
-                </div>
-                <div class="clearfix"></div> </li>
-              <li class="qa-item">
-                <div class="fl record">
-                  <div class="number">
-                    <div class="border useful">
-                      <p class="usenum"><a href="#" class="zan"><i class="fa fa-thumbs-up " aria-hidden="true"></i></a></p>
-                      <p class="zannum"> 10 </p>
-                    </div>
-                    <div class="border answer">
-                      <a href="#" class="star"><i class="fa fa-star-o" aria-hidden="true"></i></a>
-                    </div>
-                  </div>
-                </div>
-                <div class="info">
-                  <p class="text"> <a href="./spit-detail.html" target="_blank"> 面试说通过了要回去等offer，现在都两周了，还说在ceo审批，两天联系hr一次。面试说通过了，要回去等offer，现在都两周了还说在ceo审批，两天联系hr一次，都说还在等审批，什么情况？？工资什么的都谈好了，也要了工资证明。也工资证明。 </a> </p>
-                  <div class="other">
-                    <div class="fl date">
-                      <span>2017-1-22 12:02</span>
-                    </div>
-                    <div class="fr remark">
-                      <a href="#" data-toggle="modal" data-target="#shareModal" class="share"><i class="fa fa-share-alt" aria-hidden="true"></i> 分享</a>
-                      <a href="#" data-toggle="modal" data-target="#remarkModal" class="comment"><i class="fa fa-commenting" aria-hidden="true"></i> 回复</a>
-                    </div>
-                  </div>
-                </div>
-                <div class="clearfix"></div> </li>
+
             </ul>
           </div>
         </div>
@@ -222,6 +55,14 @@
 <script>
     import '~/assets/css/page-sj-spit-index.css'
     export default {
-
-  }
+        data(){
+            return{
+                url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+                srcList:[
+                    'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+                    'https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg'
+                ]
+            }
+        }
+    }
 </script>
